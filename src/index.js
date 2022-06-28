aedec55fce4b25ce496e3e9d5a09e57946ca6970
epppo <amorosso@kakao.com>
2022-06-28 21:18:15 +0900

    초기셋팅

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-
 import store from './redux/configStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
