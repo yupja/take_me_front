84f1fd50bcb0696ba93e686d12c2c141028f390c
epppo <amorosso@kakao.com>
2022-06-28 21:11:04 +0900

    FP-89 <이미지 등록을 제외한 구현 완료>

---
+++ src/components/Category.js
@@ -0,0 +1,36 @@
+import React, {useRef, useState}  from "react";
+
+import FormControl from '@mui/material/FormControl'; 
+import NativeSelect from '@mui/material/NativeSelect';
+
+const Category = (props ) => {
+
+    const handleChange = event => {
+        props.setCategory(event.target.value);
+    };
+
+    return (
+
+        <FormControl sx={{ m: 5, minWidth: 150 }} size="small">
+            <NativeSelect defaultValue={1} onChange={handleChange}>
+                <option value={1}>패션</option>
+                <option value={2}>패션잡화</option>
+                <option value={3}>화장품/미용</option>
+                <option value={4}>가구/인테리어</option>
+                <option value={5}>출산/유아동</option>
+                <option value={6}>반려동물</option>
+                <option value={7}>생활/주방용품</option>
+                <option value={8}>가전/디지털</option>
+                <option value={9}>스포츠/레저</option>
+                <option value={10}>헬스(건강)/의료용품</option>
+                <option value={11}>자동차/공구</option>
+                <option value={12}>완구/취미</option>
+                <option value={13}>문서/오피스</option>
+                <option value={14}>도서/음반</option>
+                <option value={15}>여행</option>
+            </NativeSelect>
+    </FormControl>
+    );
+}
+
+export default Category;
