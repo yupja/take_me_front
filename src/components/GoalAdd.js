d432641cec9194dcf0edd8d3b196aea3e0bf6d95
epppo <amorosso@kakao.com>
2022-07-03 14:40:51 +0900

    목표항목 작업

--- src/components/GoalAdd.js
+++ src/components/GoalAdd.js
@@ -1,40 +1,48 @@
-import React, {useRef, useState} from "react";
-import { useDispatch } from "react-redux";
+import React, {useRef, useState} from "react"
+import { useDispatch } from "react-redux"
 
-import Category from "./Category";
-import { addGoalRQ } from "../redux/modules/favorite"
+import Category from "./Category"
+import { addGoalRQ } from "../redux/modules/goal"
 
 const GoalADD = ()=>{
-    const dispatch = useDispatch();
+    const dispatch = useDispatch()
 
-    const [category , setCategory] = useState();
+    const [category , setCategory] = useState()
 
 
-    const itemName = useRef();
-    const price = useRef();
-    const goalItemCount = useRef();
+    const itemName = useRef()
+    const price = useRef()
+    const goalItemCount = useRef()
 
 
     const sendData = () =>{
         const data = {
             itemName : itemName.current.value,
             price : price.current.value,
-            goalItemCount : goalItemCount.current.value
-
+            goalItemCount : goalItemCount.current.value,
+            category : Number(category)
         }
-        dispatch(addGoalRQ(data));
+
+        dispatch(addGoalRQ(data))
     }
 
     return (
+        <>
         <div> 
             <div><Category  setCategory={setCategory}/></div>
             <div> <p>ItemName</p> <input type='text' ref={itemName}/></div>
             <div> <p>Price</p> <input type='text' ref={price}/></div> 
             <div><p>수량</p> <input type="Number" ref={goalItemCount}/></div>
-            <button onClick={sendData}>버튼이다아아아아아</button>
         </div>
+
+        <div className="footer"
+             onClick={sendData}>
+        <label>티끌만들기 완료</label>
+
+        </div>
+        </>
     )
 
 }
 
-export default GoalADD;
+export default GoalADD
---
+++ src/redux/modules/goal.js
@@ -0,0 +1,66 @@
+import { createSlice} from "@reduxjs/toolkit";
+import { instance } from "../../shared/axios";
+
+
+
+//--------------------- CREATE ---------------------------
+export const addGoalRQ = (data) =>{ // 기존에 없던 신규 목표태산 추가
+    return function (dispatch){
+        try{
+            instance.post('/goalItem',data);
+        }catch(error){
+            console.log(error)
+        }
+    }
+}
+
+
+
+//---------------------- READ ----------------------------
+export const myReadGoalRQ = () => { // 나의 태산 1개 
+    return async function (dispatch){
+        try{
+            const {data} = await instance.get('/goalItem')
+        }catch(error){
+
+        }
+     }
+}
+
+export const allReadGoalRQ = () => { // 모든 사람의 태산 항목
+    return async function(dispatch){
+        try{
+            const {data} = await instance.get('/goalItem')
+            dispatch(readeAllGoal(data))
+        }catch(error){
+
+        }
+    }
+
+}
+
+
+//-------------------- UPDATE ---------------------------
+
+
+//-------------------- DELETE ---------------------------
+
+
+
+
+//-------------------- SLICE ----------------------------
+const goalSlice = createSlice({
+    name : "goalItem",
+    initialState:{  
+        allGoalList: [],
+       },
+reducers:{
+    readeAllGoal: (state, action) => {
+        state.allGoalList = action.payload;
+      },
+
+}
+});
+
+const { readeAllGoal } = goalSlice.actions;
+export default goalSlice.reducer;
