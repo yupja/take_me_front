9869fa454a32c46ea6709dd304c70becd7f36730
epppo <amorosso@kakao.com>
2022-07-03 14:41:35 +0900

    아낌목록 메인페이지

--- src/redux/modules/saveList.js
+++ src/redux/modules/saveList.js
@@ -1,15 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { instance } from "../../shared/axios";
 
+//--------------------- CREATE ---------------------------
+export const addSaveListRQ = (data) =>{ // 내 즐겨찾기 추가 
+    // 목업은 다 집어넣음 나중에 일부 리퀘스츠 수정필요
+    return function (dispatch){
+        try{
+            instance.post('/saveList',data);
+            dispatch(addMySavedList(data));
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+}
+
+
+//---------------------- READ ----------------------------
+
+export const readSaveListRQ =() =>{
+    
+}
+
+//-------------------- UPDATE ---------------------------
+
+
+//-------------------- DELETE ---------------------------
+
+
+
+
+//-------------------- SLICE ----------------------------
 const saveSlice = createSlice({
     name : "save",
     initialState:{
-    saveList : []
+        saveList : []
 },
 reducers:{
+    readMySavedList: (state, action) => {
+        state.saveList = action.payload;
+    },
+    
+    addMySavedList : (state, action)=>{
+        state.saveList.push(action.payload);
+    }
 
 }
 });
 
-const { } = saveSlice.actions;
+const {readMySavedList, addMySavedList } = saveSlice.actions;
 export default saveSlice.reducer;
