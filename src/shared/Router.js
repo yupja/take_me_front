468986ff78b2e08e33707cc363b79127ec482a1b
eunjin <k_eunjin09@naver.com>
2022-07-05 14:36:09 +0900

    초기세팅

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Main from "../pages/Main" // 메인
 import Save from "../pages/Save";  // 아끼기
-import Mypage from "../pages/Mypage"; // 마이페이지 
+import MyPage from "../pages/MyPage"; // 마이페이지 
 import Community from "../pages/Community"; // 커뮤니티 
 import Statistics from "../pages/Statistics" // 통계
 
@@ -15,7 +15,7 @@ function Router() {
         {/* <Route path="*" element={<NotFound />} /> */}
         {/* <Route path="/main" element={<Main />} />  */}
         <Route path="/" element={<Save />} />
-        {/* <Route path="/mypage" element={<Mypage />} /> */}
+        {/* <Route path="/mypage" element={<MyPage />} /> */}
         {/* <Route path="/community" element={<Community />} /> */}
         {/* <Route path="/statistics" element={<Statistics />} /> */}
       </Routes>
