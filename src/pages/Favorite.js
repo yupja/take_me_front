fd7443bbaa799fb168ef1738255fe1f65473635b
epppo <amorosso@kakao.com>
2022-07-10 23:04:12 +0900

    no message

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 
 import { useLocation } from "react-router";
