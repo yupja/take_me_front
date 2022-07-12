849ff63b4979e4578ee0a9d919e43610db9f0f2b
epppo <amorosso@kakao.com>
2022-07-12 14:13:39 +0900

    세이브리스트 조회 컴포넌트 분리 / 해더메뉴 라우터 수정

--- src/components/CurrentSavedItem.js
+++ src/components/CurrentSavedItem.js
@@ -1,38 +1,47 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { mySavedListRQ } from "../redux/modules/saved"
+import { mySavedListRQ, deleteSavedList } from "../redux/modules/saved"
 
 import styled from "styled-components";
 import { AiOutlineStar } from 'react-icons/ai'
 
 const CurrentSavedItem =(props)=>{
     const dispatch = useDispatch();
-
-    console.log(props.goalItemId)
+    const [modifyView, setModifyView] = useState(false);
 
     useEffect(() => {
         dispatch(mySavedListRQ(props.goalItemId));
       }, [props.goalItemId]);
-
+      
 
     const mySavedList = useSelector((state) => state.saved.currentMySavedList);
     console.log(mySavedList)
-    
+
 
     return (
         
         <SavedList>
         {mySavedList&&mySavedList.data?.map((savedItem, savedItemIndex) => (
-            <>
+            <JustifyContentCenter key={savedItem.savedItemId}>
+              {modifyView? 
+              "수정이 가능해요"
+              :""}
+
               <div className="sListWrap">
-                <div className="star"><AiOutlineStar /></div>
-                <p className="date">{savedItem.createdAt}<br /></p>
-                <p>{savedItem.categoryName}</p>
-                <p>{savedItem.itemName}</p>
-                <button>수정</button>
-                <button>삭제</button>
-              </div>
-            </>
+              <div className="star"><AiOutlineStar /></div>
+                  <p className="date">
+                    {savedItem.modifiedDate.split(/[T,.]/,1)}<br/>
+                  </p>
+                  <p>{savedItem.categoryName}</p>
+                  <p>{savedItem.itemName}</p>
+                  <button onClick={()=>{
+                    setModifyView(true);
+                  }}>수정</button>
+                  <button onClick={()=>{
+                    dispatch(deleteSavedList(savedItem.savedItemId))
+                  }}>삭제</button>
+                </div>
+              </JustifyContentCenter>
         ))}
         </SavedList>
     )
@@ -40,7 +49,16 @@ const CurrentSavedItem =(props)=>{
 
 const SavedList = styled.div`
 display: flex;
+flex-direction: column;
+width: 100%;
 height: 30vh;
+
+`;
+
+const JustifyContentCenter = styled.div`
+display: flex;
+justify-content: center;
+
 `;
 
 export default CurrentSavedItem;
