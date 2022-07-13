f475b9493327ae8c3b209a776fc083120da2ae3d
epppo <amorosso@kakao.com>
2022-07-13 17:46:21 +0900

    세이브드 아이템 수정중

--- src/redux/modules/saved.js
+++ src/redux/modules/saved.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { instance } from "../../shared/axios";
-
+import { myReadGoalRQ } from "../modules/goal";
 
 
 //--------------------- CREATE ---------------------------
@@ -23,9 +23,10 @@ export const addSavedListRQ = createAsyncThunk(
 
 export const mySavedListRQ = createAsyncThunk(
   'saved/readMyList',
-  async (inputData) =>{
+  async (inputData, thunkAPI) =>{
     try {
       const {data} = await instance.get(`/api/savedItem/${inputData}`)
+      thunkAPI.dispatch(myReadGoalRQ())
       return data;
     } catch (error) {
       console.log(error);
@@ -51,14 +52,16 @@ export const loadsavedAc = (boardId) => {
 
 //-------------------- DELETE ---------------------------
 
-export const deleteSavedList = (itemId) =>{
+export const deleteSavedList = (itemId, goalItemId) =>{
   return async function (dispatch) {
     console.log(itemId)
     try{
       await instance.delete(`/api/savedItem/${itemId}`)
+      dispatch(mySavedListRQ(goalItemId))
     }catch(error){
       console.log(error);
     }
+    dispatch(myReadGoalRQ())
   }
 }
 
