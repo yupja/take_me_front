c2ab422c3cdec8d8358139d246f5c1b1afdeb41e
epppo <amorosso@kakao.com>
2022-07-20 00:37:04 +0900

    PWA 작업중

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useEffect } from "react";
-import reset from "./public/css/reset.css";
+import React from "react";
+import reset from "./styles/reset.css";
 import Router from "./shared/Router";
 import { createGlobalStyle } from "styled-components";
-import { getUserInfoDB}  from "../src/redux/modules/user"
-import { useDispatch, useSelector } from "react-redux";
+
 function App() {
   return (
-
     <div className="App">
       <GlobalStyle />
       <Router />
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux/configStore';
+import store from './store/configStore';
 import { CookiesProvider } from 'react-cookie';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+serviceWorkerRegistration.register();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
