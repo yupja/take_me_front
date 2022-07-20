e057398698bea57b51836c4cccb5691d64d3cc77
epppo <amorosso@kakao.com>
2022-07-21 02:38:51 +0900

    fix: 서비스 워커 적용불가
    서비스워커 적용 불가 / cicd 빌드 오류로 인해서 삭제처리

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { Provider } from 'react-redux';
 import store from './store/configStore';
 import { CookiesProvider } from 'react-cookie';
 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-serviceWorkerRegistration.register();
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// serviceWorkerRegistration.register();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
