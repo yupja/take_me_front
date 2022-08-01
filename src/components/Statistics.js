5edd535da5bd10250796761012b93c8f3dd7dd4a
epppo <amorosso@kakao.com>
2022-08-01 15:15:26 +0900

    fix : 날짜수정

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -19,8 +19,10 @@ function MyStatistics() {
 
   //---------------- 인풋날짜 설정----------------------------- 
   const day = new Date();
-  const inputDay = String(day.getFullYear() + "" + 0 + (day.getMonth() + 1) + "" + (day.getDate() - 1))
+  const inputDay = String(day.getFullYear() + "" + 0 + (day.getMonth() + 1) + "" + 0+ (day.getDate()))
   const inputMonth = String(day.getFullYear() + "" + 0 + (day.getMonth() + 1))
+
+  console.log(inputDay)
 
   //다른사람 태산랭크
   const allUserGoal = useSelector((state) => state.statistics.allUserGoalList);//태산목록
